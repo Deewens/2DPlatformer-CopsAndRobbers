3be72acfdbdae087b5398c9d75ec4d690ef2503e
Leo Chen <c00250593@itcarlow.ie>
2022-01-13 16:07:39 +0000

    Made the menu look nicer

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+
+    public void loadGameplay()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void loadStageOption()
+    {
+        //SceneManager.LoadScene("LevelSelector");
+        Debug.Log("Level Selection");
+    }
+
+
+    public void loadSetting()
+    {
+        Debug.Log("Setting");
+    }
+
+
+}
