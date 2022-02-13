35b734716d77dad962f06a0d8602af46b14aafa0
Leo-Chen-CM <c00250593@itcarlow.ie>
2022-02-13 23:42:11 +0000

    Made an attempt at a level 2

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,7 +7,7 @@ public class MenuController : MonoBehaviour
 
     public void loadGameplay()
     {
-        SceneManager.LoadScene("Level");
+        SceneManager.LoadScene("Level 1");
     }
 
     public void loadStageOption()
